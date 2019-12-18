a2b505f31a3c89b98cb2b1406b5bdc7e49e7a20c
Marco Nie <github@niekun.net>
2019-12-18 16:00:45 +0800

    test

--- sidebar.php
+++ sidebar.php
@@ -2,7 +2,7 @@
 <div class="col-md-3 .d-sm-none .d-md-block" id="secondary" role="complementary">
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentPosts', $this->options->sidebarBlock)) : ?>
         <section class="widget">
-            <h3 class="Display 2"><?php _e('最新文章'); ?></h3>
+            <h4 class="Display 2"><?php _e('最新文章'); ?></h4>
             <ul class="widget-list">
                 <?php $this->widget('Widget_Contents_Post_Recent')
                                             ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
@@ -12,7 +12,7 @@
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowRecentComments', $this->options->sidebarBlock)) : ?>
         <section class="widget">
-            <h3 class="Display 2"><?php _e('最近回复'); ?></h3>
+            <h4 class="Display 2"><?php _e('最近回复'); ?></h4>
             <ul class="widget-list">
                 <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
                 <?php while ($comments->next()) : ?>
@@ -24,14 +24,14 @@
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowCategory', $this->options->sidebarBlock)) : ?>
         <section class="widget">
-            <h3 class="Display 2"><?php _e('分类'); ?></h3>
+            <h4 class="Display 2"><?php _e('分类'); ?></h4>
             <?php $this->widget('Widget_Metas_Category_List')->listCategories('wrapClass=widget-list'); ?>
         </section>
     <?php endif; ?>
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowArchive', $this->options->sidebarBlock)) : ?>
         <section class="widget">
-            <h3 class="Display 2"><?php _e('归档'); ?></h3>
+            <h4 class="Display 2"><?php _e('归档'); ?></h4>
             <ul class="widget-list">
                 <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
             </ul>
@@ -40,7 +40,7 @@
 
     <?php if (!empty($this->options->sidebarBlock) && in_array('ShowOther', $this->options->sidebarBlock)) : ?>
         <section class="widget">
-            <h3 class="Display 2"><?php _e('其它'); ?></h3>
+            <h4 class="Display 2"><?php _e('其它'); ?></h4>
             <ul class="widget-list">
                 <li><a href="<?php $this->options->feedUrl(); ?>"><?php _e('文章 RSS'); ?></a></li>
                 <li><a href="<?php $this->options->commentsFeedUrl(); ?>"><?php _e('评论 RSS'); ?></a></li>
