44c9271e42125d06f67d25fed22f764a2944ef61
Marco Nie <github@niekun.net>
2019-12-18 23:38:54 +0800

    test

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-
+            <!--
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
                 <form class="form-inline" id="search" method="post" action="<?php $this->options->siteUrl(); ?>" role="search">
@@ -68,6 +68,7 @@
                     </ul>
                 <?php endif; ?>
             </div>
+            -->
         </nav>
 
         <header id="header" class="clearfix">
