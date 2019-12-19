b2368f00fa6b6a70103d6c8de31622d8c4bda5d1
Marco Nie <github@niekun.net>
2019-12-19 16:57:22 +0800

    test

--- footer.php
+++ footer.php
@@ -1,13 +1,18 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-        </div><!-- end .row -->
-    </div>
+</div><!-- end .row -->
+</div>
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
     &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. All rights reserved. Power by <a href="http://typecho.org">Typecho.</a> Theme designed by <a href="https://niekun.net">Marco Nie.</a>
 </footer><!-- end #footer -->
 
+<script type="text/javascript">
+    navItemSytle();
+</script>
+
 <?php $this->footer(); ?>
 </body>
+
 </html>
