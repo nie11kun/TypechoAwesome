40918be263d6b016f1e89b0da9ed44e1b1e6e5fe
Marco Nie <github@niekun.net>
2019-12-19 09:18:40 +0800

    add foot info

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
 </div><!-- end #body -->
 
 <footer id="footer" role="contentinfo">
-    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. All rights reserved.
+    &copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?></a>. All rights reserved. Power by<a href="http://typecho.org">Typecho.</a>
 </footer><!-- end #footer -->
 
 <?php $this->footer(); ?>
