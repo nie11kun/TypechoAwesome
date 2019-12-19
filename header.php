98ac413b1f1839d7b699eecf540f8939591ef5e8
Marco Nie <github@niekun.net>
2019-12-19 09:13:58 +0800

    test

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
     <script src="https://niekun.net/webcdn//popper.js-1.16.0/popper.min.js"></script>
     <script src="https://niekun.net/webcdn/bootstrap-4.4.1-dist/js/bootstrap.min.js"></script>
 
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('normalize.css?v=1'); ?>">
+    <link rel="stylesheet" href="<?php $this->options->themeUrl('normalize.css?v=2'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('style.css?v=2'); ?>">
 
     <!--[if lt IE 9]>
